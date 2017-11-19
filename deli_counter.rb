4762ccfcce53192ce36dcc44938ebac458eab409
Larissa S <larissafschiavo@gmail.com>
2017-11-19 06:04:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,5 +22,6 @@ def now_serving(counter_line)
   if counter_line.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Now serving #{counter_line[0]}"
+    puts "Now serving #{counter_line[0]}."
+    counter_line.unshift 
 end
